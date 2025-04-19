c94b29553b66e1bded861a3ae65a8cb7d3a01221
LIFLA MARY L K <124064755+liflamary@users.noreply.github.com>
2025-04-19 18:14:33 +0530

    Add files via upload

---
+++ Shop.js
@@ -0,0 +1,14 @@
+import './Shop.css';
+import HeroSection from './components/HeroSection/HeroSection';
+import Navigation from './components/Navigation/Navigation';
+
+const App=()=>{
+  return (
+    <div >
+      <Navigation/>
+      <HeroSection/>
+    </div>
+  );
+}
+
+export default App;
